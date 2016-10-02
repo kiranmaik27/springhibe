94f22963ad62ccb7b60c43330638893735879f27
Kiranmai <kiranmai.k27@gmail.com>
2016-10-02 11:14:18 -0400

    Mapping Strategies and hibernate queries

--- src/main/java/com/hibernate/PersonHibernateService.java
+++ src/main/java/com/hibernate/PersonHibernateService.java
@@ -1,14 +1,19 @@
 package com.hibernate;
 
+import com.hibernate.mapping.Toyota;
 import com.hibernate.mapping.Vehicle;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.NativeQuery;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * Created by aditya on 9/23/2016.
@@ -52,5 +57,67 @@ public class PersonHibernateService{
 
     public void create(Vehicle vehicle){
         sessionFactory.getCurrentSession().saveOrUpdate(vehicle);
+    }
+
+    /**
+     * @param id
+     * @return
+     */
+    public Vehicle getVehicleById(Long id) {
+        return sessionFactory.getCurrentSession().load(Vehicle.class, id);
+    }
+
+    public List<Vehicle> getAllByVin(String vinStart) {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        String hql = "from Vehicle v where v.vin like '" + vinStart + "%'";
+
+
+        Query query = session.createQuery(hql);
+
+        List<Vehicle> vehicles = query.list();
+
+        return vehicles;
+
+
+    }
+
+
+    public List<Vehicle> getHondaByModelAndYear(String model, String year) {
+        Session session = sessionFactory.getCurrentSession();
+        Query query = session.createQuery("from Honda h where h.model=:model and h.year=:year")
+                .setParameter("model", model)
+                .setParameter("year", year);
+
+        return query.list();
+
+
+    }
+
+    public List<Vehicle> getToyotaByModelAndYear(String model, String year) {
+        Session session = sessionFactory.getCurrentSession();
+        Query query = session.getNamedQuery("query.getToyotaByModelAndYear")
+                .setParameter("model", model)
+                .setParameter("year", year);
+        return query.list();
+
+
+    }
+
+
+    public List<Vehicle> getAllToyotaSQL(String model, String year) {
+        Session session = sessionFactory.getCurrentSession();
+        NativeQuery nativeQuery = session.getNamedNativeQuery("query.sql.toyota")
+                .setParameter("model", model)
+                .setParameter("year", year);
+
+        nativeQuery.addEntity(Toyota.class);
+
+        List<Vehicle> all = nativeQuery.list();
+
+        return all;
+
+
     }
 }
--- src/test/java/com/hibernate/MappingHibernateTest.java
+++ src/test/java/com/hibernate/MappingHibernateTest.java
@@ -1,6 +1,8 @@
 package com.hibernate;
 
 import com.hibernate.mapping.Honda;
+import com.hibernate.mapping.Toyota;
+import com.hibernate.mapping.Vehicle;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,9 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import java.util.List;
 
 /**
  * Created by Kiranmai on 10/1/16.
@@ -31,6 +36,41 @@ public class MappingHibernateTest {
 
         personHibernateService.create(honda);
 
+        Toyota toyota = new Toyota();
+        toyota.setModel("Camry");
+        toyota.setYear("2016");
+        toyota.setVin("23234323534543");
 
+        personHibernateService.create(toyota);
+    }
+
+    @Test
+    public void testGet() {
+        Vehicle vehicle = personHibernateService.getVehicleById(1L);
+        Assert.notNull(vehicle);
+    }
+
+    @Test
+    public void testHQL() {
+        List<Vehicle> vehicles = personHibernateService.getAllByVin("1");
+        Assert.notNull(vehicles);
+    }
+
+    @Test
+    public void testGetHonda() {
+        List<Vehicle> vehicles = personHibernateService.getHondaByModelAndYear("CRV", "2016");
+        Assert.notNull(vehicles);
+    }
+
+    @Test
+    public void testGetToyota() {
+        List<Vehicle> vehicles = personHibernateService.getToyotaByModelAndYear("CAMRY", "2017");
+        Assert.notNull(vehicles);
+    }
+
+    @Test
+    public void testGetToyotaSQL() {
+        List<Vehicle> vehicles = personHibernateService.getAllToyotaSQL("CAMRY", "2017");
+        Assert.notNull(vehicles);
     }
 }
